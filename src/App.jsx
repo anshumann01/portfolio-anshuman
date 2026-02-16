ed47fc29d6c102074eb8c3bd1b32a875a21911fc
anshumann01 <anshumankrishna01@gmail.com>
2026-02-16 23:19:11 +0530

    Fixed Case Sensitivity Issue while Deploying

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import Navbar from './Components/navbar'
+import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
 import About from './Components/About'
 import Skills from './Components/Skills'
